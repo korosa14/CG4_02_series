1ae23ba9bdfb46c8d8b45843d4ac9d1df74872e1
katuragi uto <k022g0137@g.neec.ac.jp>
2024-05-16 10:21:12 +0900

    Player Script完了

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,30 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveSpeed = 5.0f;
+        Vector3 v = rb.velocity;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            v.x = moveSpeed;
+        }
+        else
+        {
+            v.x = 0;
+        }
+
+        rb.velocity = v;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x = -moveSpeed;
+        }
+        rb.velocity = v;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            v.y = moveSpeed;
+        }
+        rb.velocity = v;
     }
 }
