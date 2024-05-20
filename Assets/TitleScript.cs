7860d1bf25d90947366362aa363782210eba3c92
katuragi uto <k022g0137@g.neec.ac.jp>
2024-05-20 14:46:59 +0900

    タイトル点滅表示完了

--- Assets/TitleScript.cs
+++ Assets/TitleScript.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class TitleScript : MonoBehaviour
 {
+    public GameObject hitKey;
+
+    private int timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,16 @@ public class TitleScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("SampleScene");
+        }
+        //タイマー点滅
+        timer++;
+        if(timer%100>50)
+        {
+            hitKey.SetActive(false);
+        }
+        else
+        {
+            hitKey.SetActive(true);
         }
     }
 }
